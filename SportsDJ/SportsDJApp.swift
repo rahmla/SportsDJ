bb38ce669bb7e40cf88ee391d8e2aa3fb579cf4e
Lars Rahm <lars.rahm72@gmail.com>
2026-03-26 22:06:27 +0100

    Initial Commit

---
+++ SportsDJ/SportsDJApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsDJApp.swift
+//  SportsDJ
+//
+//  Created by Lars Rahm on 2026-03-26.
+//
+
+import SwiftUI
+
+@main
+struct SportsDJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
